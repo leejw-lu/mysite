c8941ffef0292b07162929ede9e125b3f10bb6c5
leejw-lu <jiwoojenny@naver.com>
2024-06-07 17:07:47 +0900

    class: mysite3 guestbook mybatis @ Transactional

---
+++ database/mysite.sql
@@ -0,0 +1,31 @@
+-- guestbook_log
+update guestbook_log set count=count+1 where date = current_date();
+insert into guestbook_log values(current_date(), 1);
+
+-- select date(reg_date) from guestbook where no=32 ;
+update guestbook_log set count=count-1 where date = (select date(reg_date) from guestbook where no=32 );
+
+select * from guestbook;
+select * from guestbook_log;
+
+-- mysite sql
+insert into board values(null, 'Hi', 'contents', 0, now(), (select max(g_no)+1 from board) , 1, 0, 4);
+insert into board select null, 'Hi', 'contents', 0, now(), max(g_no)+1 , 1, 0, 4 from board;
+
+select max(g_no)+1 from board;
+
+select a.name, b.no, b.title, b.contents, b.hit, date_format(b.reg_date, '%Y-%m-%d %H:%i:%s'), b.g_no, b.o_no, b.depth from user a, board b where a.no=b.user_no order by b.g_no desc, b.o_no asc limit 6 , 3;
+
+update board set hit=hit+1 where no= ?;
+
+select * from user;
+select * from board;
+
+select a.name, b.no, b.title, b.contents, b.hit, b.reg_date, b.g_no, b.o_no, b.depth from user a, board b where a.no=b.user_no limit 3 , 3;
+select count(*), ceil(count(*)/3) from board;
+
+insert into board values(null, 'Test', 'contents', 0, now(), 1, 1, 1, 7);
+insert into user values(null, '123','123', password('123'), 'female', current_date());
+
+insert into board select null, ?, ?, ?, now(), max(g_no)+1 , ?, ?, ? from board
+
--- mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.poscodx.mysite.repository.GuestbookLogRepository;
 import com.poscodx.mysite.repository.GuestbookRepository;
 import com.poscodx.mysite.vo.GuestbookVo;
 
@@ -14,15 +16,26 @@ public class GuestbookService {
 	@Autowired
 	private GuestbookRepository guestbookRepository;
 	
+	@Autowired
+	private GuestbookLogRepository guestbookLogRepository;
+	
 	public List<GuestbookVo> getContentsList() {
 		return guestbookRepository.findAll();
 	}
 	
+	@Transactional
 	public void deleteContents(GuestbookVo vo) {
+		guestbookLogRepository.update(vo.getNo());
 		guestbookRepository.deleteByNoAndPassword(vo);
 	}
 	
-	public void addContents(GuestbookVo vo) {
+	@Transactional
+	public void addContents(GuestbookVo vo) {		
+		int count= guestbookLogRepository.update();
+		if (count==0) {
+			guestbookLogRepository.insert();
+		}
+
 		guestbookRepository.insert(vo);
 	}
 	
