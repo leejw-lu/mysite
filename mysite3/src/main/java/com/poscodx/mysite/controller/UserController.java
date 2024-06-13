9e6ba485160afde5c24cc7f94b7461a943f81e00
leejw-lu <jiwoojenny@naver.com>
2024-06-13 10:56:20 +0900

    class: BE validation 및 message source

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.poscodx.mysite.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,8 +28,18 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo vo) {
-		userService.join(vo);
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			//model.addAttribute("userVo", vo);
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error: list) {
+//				System.out.println(error);
+//			}
+			model.addAllAttributes(result.getModel());
+
+			return "user/join";
+		}
+		//userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
