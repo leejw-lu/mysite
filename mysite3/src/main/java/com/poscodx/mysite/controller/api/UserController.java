33f275ae6862f439da8b8e81b14ec0c69c2b3757
leejw-lu <jiwoojenny@naver.com>
2024-06-17 13:17:13 +0900

    class: JsonResult (dto 사용) 및 예외 발생 시 json 응답

--- mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.poscodx.mysite.dto.JsonResult;
 import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
 
@@ -19,8 +20,8 @@ public class UserController {
 	
 	//@ResponseBody
 	@RequestMapping("/checkemail")
-	public Object checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
-		UserVo vo = userService.getUser(email);
-		return Map.of("exist", vo != null);
+	public JsonResult checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
+		UserVo vo = userService.getUser(email);		
+		return JsonResult.success(vo!=null);
 	}
 }
