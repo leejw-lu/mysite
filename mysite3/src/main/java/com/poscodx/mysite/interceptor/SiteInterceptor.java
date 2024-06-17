f94049aa830e1fe33cc6742b83fe713cbf525a9f
leejw-lu <jiwoojenny@naver.com>
2024-06-17 15:19:22 +0900

    class: locale (message source) 적용 및 Cookie Locale Resolver

--- mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
+++ mysite3/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
@@ -5,11 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
 
 import com.poscodx.mysite.service.SiteService;
 import com.poscodx.mysite.vo.SiteVo;
 
 public class SiteInterceptor implements HandlerInterceptor {
+	@Autowired
+	private LocaleResolver localeResolver;
+	
 	@Autowired
 	private SiteService siteService;
 	
@@ -22,7 +26,11 @@ public class SiteInterceptor implements HandlerInterceptor {
 		if(siteVo==null) {
 			siteVo=siteService.getSite();
 			request.getServletContext().setAttribute("sitevo", siteVo);
-		}		
+		}
+		
+		//Locale
+		System.out.println("resolver-locale:" + localeResolver.resolveLocale(request).getLanguage());		
+		request.setAttribute("language", localeResolver.resolveLocale(request).getLanguage());
 		return true;
 	}
 
