693f934a9b2334297692a0148e7cc39ccad7d688
leejw-lu <jiwoojenny@naver.com>
2024-06-19 14:32:57 +0900

    class: mysite5 NoHanderFoundException 처리

--- mysite5/src/main/java/com/poscodx/mysite/initializer/MySiteWebApplicationInitializer.java
+++ mysite5/src/main/java/com/poscodx/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -2,7 +2,10 @@ package com.poscodx.mysite.initializer;
 
 import javax.servlet.Filter;
 
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.poscodx.mysite.config.AppConfig;
@@ -29,8 +32,13 @@ public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDis
 	protected Filter[] getServletFilters() {
 		return new Filter[] {new CharacterEncodingFilter("UTF-8")};
 	}
-	
-	
+
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet servlet= (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+		servlet.setThrowExceptionIfNoHandlerFound(true);
+		return servlet;
+	}
 
 	
 }
